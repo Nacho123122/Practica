a76e5e24a758b70a006bab3b1e3e069a01bba368
Nacho123122 <juanignaciojimenezcondori122@gmail.com>
2024-01-16 23:24:44 -0400

    practica

---
+++ ejercicio-1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Boton from './components/Boton';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Boton />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicio-1/src/components/Boton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../style/Boton.css';
+
+function Boton(){
+    return(
+        <div className="show">
+            <div className="boton">
+            <button>
+                Show\Hide
+            </button>
+            </div>
+            <h1>
+                Welcome to the React Challenges
+            </h1>
+        </div>
+    );
+}
+
+export default Boton;
